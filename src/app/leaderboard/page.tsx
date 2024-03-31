bd7dd3b6ffeaee4c21eb0f030e39cc00253b26cb
shanecranor <shanecranor@gmail.com>
2024-03-31 16:04:51 -0500

    add leaderboard page

---
+++ src/app/leaderboard/page.tsx
@@ -0,0 +1,15 @@
+import "./page.scss";
+import React from "react";
+
+export default function Home() {
+  return (
+    <main className="p-leaderboard">
+      <h1>Leaderboard</h1>
+      <p>
+        Welcome to the leaderboard! Here you can see how you stack up against
+        the competition. The leaderboard is updated every day around 9:00 PM
+        MST. You can also view the leaderboard for previous days.
+      </p>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,25 @@
+import Link from "next/link";
 import "./page.scss";
 import React from "react";
 
 export default function Home() {
   return (
     <main className="p-home">
-      <div className="content">
-        <h1>GeoCup</h1>
-        <div className="welcome">
-          <p>
-            Welcome to GeoCup! The rules are simple: over the course of April
-            2024, we'll play a game of GeoGuessr every day. Everyone is welcome
-            and a challenge link will be available on this page every day, as
-            well as links from previous days.
-          </p>
-          <p>
-            <strong>
-              Results will be uploaded to the site around 9:00 PM MST every day.
-            </strong>{" "}
-            You can still play afterwards, you just may not show up on the live
-            <a href="/leaderboard"> leaderboard</a>.
-          </p>
-        </div>
+      <h1>GeoCup</h1>
+      <div className="welcome">
+        <p>
+          Welcome to GeoCup! The rules are simple: over the course of April
+          2024, we'll play a game of GeoGuessr every day. Everyone is welcome
+          and a challenge link will be available on this page every day, as well
+          as links from previous days.
+        </p>
+        <p>
+          <strong>
+            Results will be uploaded to the site around 9:00 PM MST every day.
+          </strong>{" "}
+          You can still play afterwards, you just may not show up on the live
+          <Link href="/leaderboard"> leaderboard</Link>.
+        </p>
       </div>
     </main>
   );
